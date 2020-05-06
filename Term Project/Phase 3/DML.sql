9caf6320f617b451344afdfb0f212763be7e579e
Jon <jondwoo@gmail.com>
2020-05-05 19:57:47 -0700

    add remaining FK constraints to shift

--- Term Project/Phase 3/DML.sql
+++ Term Project/Phase 3/DML.sql
@@ -1,1 +1,8 @@
+use mimings_cuisine;
 
+ALTER TABLE shift 
+ADD CONSTRAINT shift_empShift_fk04 FOREIGN KEY (managerID, shiftDate, dayType) REFERENCES empShift(empID, shiftDate, dayType),
+ADD CONSTRAINT shift__empShift_fk05 FOREIGN KEY (chefID, shiftDate, dayType) REFERENCES empShift(empID, shiftDate, dayType);
+
+-- SET FOREIGN_KEY_CHECKS=0; when inserting into shift and empshift
+-- SET FOREIGN_KEY_CHECKS=1; after done
